a2d370614011efb36dff287a424eb981e115d752
Mikey Turck <mikeyturck@Mikeys-MacBook-Pro.local>
2018-03-10 15:37:57 -0500

    progress with builder

--- platform/prototype.js
+++ platform/prototype.js
@@ -1,21 +1,23 @@
-var data = '{"user":"testing","projectID":"12345","sceneData":{"object1":{"name":"floor","geometry":"plane","properties":{"color":"#7BC8A4","position":"0 0 0","rotation":"-90 0 0","width":"8","height":"8"}},"object2":{"name":"sphere","geometry":"shere","properties":{"color":"#ff0000","position":"0 0 -4","radius":"2"}}}}'
+var data = '{"user":"testing","projectID":"12345","sceneData":{"object1":{"name":"floor","geometry":"plane","properties":{"src":"graph.jpg","position":"0 0 -4","rotation":"-90 0 0","width":"8","height":"8"}},"object2":{"name":"sky","geometry":"sky","properties":{"color":"#dddddd"}}}}'
 var json = JSON.parse(data)
+var sd = json.sceneData
 
 // scene element
+function renderScene(d) {
 var scene = document.querySelector("a-scene")
 
 // loop through all objects
-var sd = json.sceneData
-var objKeys = Object.keys(sd)
+
+var objKeys = Object.keys(d)
 for (x in objKeys) {
 
   // create a-frame element
-  geo = sd[objKeys[x]].geometry
+  geo = d[objKeys[x]].geometry
   el = document.createElement(processGeo(geo))
   scene.appendChild(el)
 
   // set a-frame element properties
-  p = sd[objKeys[x]].properties
+  p = d[objKeys[x]].properties
   propKeys = Object.keys(p)
   console.log(propKeys);
   for (y in propKeys) {
@@ -24,6 +26,8 @@ for (x in objKeys) {
     console.log(p[propKeys[y]])
 }
 }
+}
+renderScene(sd);
 
 
 function processGeo(geo) {
